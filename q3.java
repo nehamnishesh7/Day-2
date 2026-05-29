b6c37a6f82cc06c042d155a8ad393c3f712713ec
nehamnishesh7 <nisheshneham7@gmail.com>
2026-05-29 20:02:26 +0530

    Update q3.java

--- q3.java
+++ q3.java
@@ -4,7 +4,6 @@ public class TravelExpenseEstimator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Taking inputs
         System.out.print("Enter travel distance (in KM): ");
         double distance = scanner.nextDouble();
 
@@ -14,11 +13,9 @@ public class TravelExpenseEstimator {
         System.out.print("Enter vehicle mileage (KM per liter): ");
         double mileage = scanner.nextDouble();
 
-        // Calculating petrol needed and total cost
         double petrolNeeded = distance / mileage;
         double totalCost = petrolNeeded * petrolPrice;
 
-        // Outputting the result
         System.out.println("\n--- Travel Expense Estimation ---");
         System.out.println("Petrol needed: " + petrolNeeded + " liters");
         System.out.println("Total travel cost: " + totalCost);
