0e4e0b9344a0dd12c66492dbe1d1631a552387cb
nehamnishesh7 <nisheshneham7@gmail.com>
2026-05-29 20:01:42 +0530

    Update q2.java

--- q2.java
+++ q2.java
@@ -4,7 +4,7 @@ public class StudentResultAnalyzer {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Taking inputs
+     
         System.out.print("Enter Student Name: ");
         String studentName = scanner.nextLine();
 
@@ -17,17 +17,14 @@ public class StudentResultAnalyzer {
         System.out.print("Enter marks for Subject 3: ");
         double marks3 = scanner.nextDouble();
 
-        // Calculating total and average
         double totalMarks = marks1 + marks2 + marks3;
         double averageMarks = totalMarks / 3;
 
-        // Outputting the result
         System.out.println("\n--- Result Details ---");
         System.out.println("Student Name: " + studentName);
         System.out.println("Total Marks: " + totalMarks);
         System.out.println("Average Marks: " + averageMarks);
 
-        // Bonus: Comparison operator
         boolean isPassing = averageMarks > 50;
         System.out.println("Is average greater than 50? " + isPassing);
 
